23a60f76a51458b8bb5d214a447b08a88b0657f9
Venelin Petkov <petkov.venelin@gmail.com>
2018-05-26 13:12:46 +0200

    more commands

--- aws/prepare.sh
+++ aws/prepare.sh
@@ -3,7 +3,19 @@
 export DEBIAN_FRONTEND=noninteractive
 
 #
+sudo apt-get update
+
+#
+sudo apt-get install -y python3-pip
+
+#
 sudo apt-get install -y xorg-dev
+
+#
+sudo apt-get install -y python3-tk
+
+#
+sudo apt-get install unzip
 
 #
 wget http://us.download.nvidia.com/XFree86/Linux-x86_64/390.48/NVIDIA-Linux-x86_64-390.48.run
@@ -15,13 +27,10 @@ chmod +x NVIDIA-Linux-x86_64-390.48.run
 sudo ./NVIDIA-Linux-x86_64-390.48.run  --accept-license --no-opengl-files --silent
 
 #
-sudo apt-get install -y python3-pip
+pip3 install http://download.pytorch.org/whl/cu91/torch-0.4.0-cp36-cp36m-linux_x86_64.whl
 
 #
-pip3 install http://download.pytorch.org/whl/cu91/torch-0.4.0-cp35-cp35m-linux_x86_64.whl 
+sudo pip3 install -y torchvision
 
 #
-pip3 install torchvision
-
-#
-pip3 install fastai
+sudo pip3 install -y fastai
